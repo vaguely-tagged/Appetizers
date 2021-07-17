e884d820d8efead059da1f9c1459f322691b9840
Vincent Sgherzi <sgherzivincent@gmail.com>
2021-07-17 13:10:06 +0900

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Vince Sgherzi on 7/17/21.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Appetizers/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Appetizers
+//
+//  Created by Vince Sgherzi on 7/17/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
